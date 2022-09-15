2f8e1eb2b7724eb2147dce94b8cd3119648a2a83
Kieran O'Neill <kieran.oneill@pm.me>
2022-09-15 12:21:46 +0200

    fix: update plugin class to the latest version

--- src/nettle-pay.php
+++ src/nettle-pay.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Nettle Pay
  * Plugin URI:        https://nettlelabs.com/pay
  * Description:       Nettle makes it easy to take crypto payments globally, reward customers and reduce your CO2e emissions, all in one place.
- * Version:           1.0.0
+ * Version:           1.2.0
  * Author:            Nettle Labs Ltd
  * Author URI:        https://nettlelabs.com
  * License:           GPL-2.0+
